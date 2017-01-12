557696660638c771e2f32d32675dc98f28608dfc
Kyle Soule <kyle.soule@gmail.com>
2017-01-12 14:49:12 -0600

    Champion Lookups
    Added champion (by ID) lookup interfaces.

---
+++ Objective.py
@@ -0,0 +1,24 @@
+"""
+Objective:
+    1. What champion performs the best in existing team comp
+    2. What are the chances of victory based on team comps
+
+Method:
+    Seed program with summoners from different ranks
+    Branch off original summoner
+    Consider game mode played (ARAM, ranked, normals)
+
+Data:
+    Mass collection of data
+    Parse specific data points
+    Store raw JSON for future analysis
+
+TODO:
+    Task            Objective(s)
+    ----            ------------
+    Team ID         1
+    Winner          1
+    Champion ID     1
+    Summoner ID     1
+    Dump JSON       *
+"""
--- riot_lib.py
+++ riot_lib.py
@@ -1,6 +1,14 @@
 import json, urllib2, sys
 import debug as Debugger
 
+RATE_LIMIT = 500    # per 10 minutes
+WAIT_TIME = ((600.0 / RATE_LIMIT) + 0.5)
+
+"""
+TODO:
+    IMPLEMENT A RATE LIMITER ON ALL CALLS!
+"""
+
 class Riot:
     api_key = "fb86f255-edcf-4b8f-ba07-8b0f675c8b51"
     url = "https://na.api.pvp.net/api/lol/na/"
@@ -62,7 +70,7 @@ class Riot:
             dbg.report_error("Summoner name not found!")
             sys.exit()
             
-    def get_recent_games_by_id(self, id):
+    def get_recent_game_data_by_summoner_id(self, id):
         """
         Retrieve a list of the last 10 games by summoner ID.
         
@@ -75,7 +83,6 @@ class Riot:
         See Also:
             https://developer.riotgames.com/api/methods#!/1207/4679
         """
-        games = []
         a_url = "v1.3/game/by-summoner/"
         s_url = "{self_url}{api_url}{id}/recent?api_key={api_key}".format(self_url=self.url,
                                                                    api_url=a_url,
@@ -86,23 +93,123 @@ class Riot:
         data = json.loads(response.read())
         
         # Debug data
-        dbg.add_msg("get_recent_games_by_id(self, id)", "a_url", a_url, 4)
-        dbg.add_msg("get_recent_games_by_id(self, id)", "s_url", s_url, 4)
-        dbg.print_msg("get_recent_games_by_id(self, id)")
+        dbg.add_msg("get_recent_game_data_by_summoner_id(self, id)", "a_url", a_url, 4)
+        dbg.add_msg("get_recent_game_data_by_summoner_id(self, id)", "s_url", s_url, 4)
+        dbg.print_msg("get_recent_game_data_by_summoner_id(self, id)")
         
         try:
-            for game in data["games"]:
-                games.append(game["gameId"])
-            return games
+            return data
         except:
             dbg.report_error("No games found!")
             sys.exit()
+    
+    def get_champion_name_by_id(self, championId):
+        """
+        Returns a full list of champions by ID.
+        
+        Args:
+            id: Champion ID
+        
+        Returns:
+            Champion name
+            
+        See Also:
+            https://developer.riotgames.com/api/methods#!/1055/3622
+        """
+        static_url = "https://global.api.pvp.net/api/lol/static-data/na/"
+        a_url = "v1.2/champion/"
+        s_url = "{static_url}{api_url}{id}?api_key={api_key}".format(static_url=static_url,
+                                                                     api_url=a_url,
+                                                                     id=championId,
+                                                                     api_key=self.api_key)
+        
+        # Debug data
+        dbg.add_msg("get_champion_name_by_id(self, championId)", "static_url", static_url, 4)
+        dbg.add_msg("get_champion_name_by_id(self, championId)", "a_url", a_url, 4)
+        dbg.add_msg("get_champion_name_by_id(self, championId)", "s_url", s_url, 4)
+        dbg.print_msg("get_champion_name_by_id(self, championId)")
+        
+        try:
+            response = urllib2.urlopen(s_url)
+            data = json.loads(response.read())
+            return data["name"]
+        except:
+            return "Champion not found!"
+    
+    def get_full_champion_list(self):
+        """
+        Returns a complete list of all champions by ID.
+        
+        Returns:
+            Champion ID
+            Name
+        
+        See Also:
+            https://developer.riotgames.com/api/methods#!/1055/3633
+        https://global.api.pvp.net/api/lol/static-data/na/v1.2/champion?api_key=fb86f255-edcf-4b8f-ba07-8b0f675c8b51
+        """
+        champions = []
+        static_url = "https://global.api.pvp.net/api/lol/static-data/na/"
+        a_url = "v1.2/champion/"
+        s_url = "{static_url}{api_url}?api_key={api_key}".format(static_url=static_url,
+                                                                 api_url=a_url,
+                                                                 api_key=self.api_key)
+        
+        response = urllib2.urlopen(s_url)
+        data = json.loads(response.read())
+        
+        # Debug data
+        dbg.add_msg("get_full_champion_list(self)", "static_url", static_url, 4)
+        dbg.add_msg("get_full_champion_list(self)", "a_url", a_url, 4)
+        dbg.add_msg("get_full_champion_list(self)", "s_url", s_url, 4)
+        dbg.print_msg("get_full_champion_list(self)")
+        
+        for champIdName in data["data"]:
+            champion = data["data"][champIdName]
+            champions.append([champion["id"], champion["name"]])
+        
+        return champions
         
 
-dbg = Debugger.Debug(Debugger.INFORMATION)
+dbg = Debugger.Debug(Debugger.WARNING)
 riot_api = Riot()
-id = riot_api.get_summoner_by_name("SmashBrethren")
-games = riot_api.get_recent_games_by_id(id)
 
-for game in games:
-    print game
+champions = riot_api.get_full_champion_list()
+
+for champion in champions:
+    print("{id}\t{name}".format(id=champion[0], name=champion[1]))
+
+
+
+"""
+id = riot_api.get_summoner_by_name("SmashBrethren")
+games = riot_api.get_recent_game_data_by_summoner_id(id)
+"""
+
+
+"""
+seedId = games["summonerId"]
+for game in games["games"]:
+    players = []
+    gameId = game["gameId"]
+    
+    if game["stats"]["team"] == 100:
+        if game["stats"]["win"] == True:
+            winner = 100
+        else:
+            winner = 200
+    else:
+        if game["stats"]["win"] == True:
+            winner = 200
+        else:
+            winner = 100
+    
+    print("Winner of {gameId} is {winner}:".format(gameId=gameId, winner=winner))
+    
+    players.append([game["teamId"], game["championId"], seedId])
+    for player in game["fellowPlayers"]:
+        players.append([player["teamId"], player["championId"], player["summonerId"]])
+    
+    for player in players:
+        print("\t{t}\t{c}\t{s}".format(t=player[0], c=player[1], s=player[2]))
+"""
